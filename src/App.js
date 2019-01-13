26da4637f1a16d5a891459b9e4f27f595b0ecc44
baumhoto <baumhoto@gmail.com>
2019-01-13 10:48:35 +0100

    static list with title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
+
 class App extends Component {
   render() {
-    const helloWord = 'Welcome to the Road to learn React'
     return (
       <div className="App">
-        <h2>{helloWord}</h2>
+        {list.map(function(item) {
+          return <div>{item.title}</div>;
+        })}
       </div>
     );
   }
